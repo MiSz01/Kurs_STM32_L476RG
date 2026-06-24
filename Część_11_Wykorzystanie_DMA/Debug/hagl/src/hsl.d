b87032f53a2c7940e629007f3283bff4bd8bba30
MiSz01 <milosz12212321@gmail.com>
2026-06-24 14:58:51 +0200

    Korekta nazwy projektu

--- Część_11_Wykorzystanie_DMA/Debug/hagl/src/hsl.d
+++
@@ -1,7 +0,0 @@
-hagl/src/hsl.o: ../hagl/src/hsl.c \
- C:/Users/02/Desktop/STM32/Praktykanci/Część_11_Wykorzystanie_DMA/hagl/include/hsl.h \
- C:/Users/02/Desktop/STM32/Praktykanci/Część_11_Wykorzystanie_DMA/hagl/include/rgb888.h \
- C:/Users/02/Desktop/STM32/Praktykanci/Część_11_Wykorzystanie_DMA/hagl/include/hsl.h
-C:/Users/02/Desktop/STM32/Praktykanci/Część_11_Wykorzystanie_DMA/hagl/include/hsl.h:
-C:/Users/02/Desktop/STM32/Praktykanci/Część_11_Wykorzystanie_DMA/hagl/include/rgb888.h:
-C:/Users/02/Desktop/STM32/Praktykanci/Część_11_Wykorzystanie_DMA/hagl/include/hsl.h:
--- Część_11_Wykorzystanie_DMA/Debug/hagl/src/rgb565.d
+++
@@ -1,7 +0,0 @@
-hagl/src/rgb565.o: ../hagl/src/rgb565.c \
- C:/Users/02/Desktop/STM32/Praktykanci/Część_11_Wykorzystanie_DMA/hagl/include/rgb565.h \
- C:/Users/02/Desktop/STM32/Praktykanci/Część_11_Wykorzystanie_DMA/hagl/include/rgb888.h \
- C:/Users/02/Desktop/STM32/Praktykanci/Część_11_Wykorzystanie_DMA/hagl/include/hsl.h
-C:/Users/02/Desktop/STM32/Praktykanci/Część_11_Wykorzystanie_DMA/hagl/include/rgb565.h:
-C:/Users/02/Desktop/STM32/Praktykanci/Część_11_Wykorzystanie_DMA/hagl/include/rgb888.h:
-C:/Users/02/Desktop/STM32/Praktykanci/Część_11_Wykorzystanie_DMA/hagl/include/hsl.h:
